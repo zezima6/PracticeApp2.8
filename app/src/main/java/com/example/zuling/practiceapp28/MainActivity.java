d3caa6553e64671db0ee7edd9ed49c9a76090785
Zuling <zulingc6@gmail.com>
2017-10-24 13:18:30 -0500

    Start page

---
+++ app/src/main/java/com/example/zuling/practiceapp28/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.zuling.practiceapp28;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Zuling on 10/24/2017.
+ */
+
+public class MainActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.startpage);
+
+    }
+}
