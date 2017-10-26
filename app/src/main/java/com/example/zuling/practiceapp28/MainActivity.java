c4afba109595c3accf80d18bf5143bdd3f6f84d2
Zuling <zulingc6@gmail.com>
2017-10-26 12:41:40 -0500

    Start page

--- app/src/main/java/com/example/zuling/practiceapp28/MainActivity.java
+++ app/src/main/java/com/example/zuling/practiceapp28/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.zuling.practiceapp28;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by Zuling on 10/24/2017.
@@ -9,9 +11,14 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity{
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.startpage);
+    }
+    public void sendMessage (View view){
+        Intent startNewActivity = new Intent(this, MapsActivity.class);
+        startActivity(startNewActivity);
 
     }
-}
+    }
+
